2b8d2ba2ff3ca09195d997310a24312471da0063
unknown <ankurguptajp@gmail.com>
2024-02-04 12:33:39 +0530

    Change endpoint

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from datetime import timedelta
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://dbeditor:aPk3ClBYXa@43.205.70.36:5432/template1'
-MONGO_URI = 'mongodb://43.205.70.36:27017/test'
+SQLALCHEMY_DATABASE_URI = 'postgresql://dbeditor:aPk3ClBYXa@10.0.12.64:5432/template1'
+MONGO_URI = 'mongodb://10.0.12.64:27017/test'
 SECRET_KEY = 'your_secret_key'
 
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
