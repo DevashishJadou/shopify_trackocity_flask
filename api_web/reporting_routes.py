56bf03b430a93a1ef4e2c1ba89887ed2b15b4424
unknown <ankurguptajp@gmail.com>
2024-03-16 09:22:54 +0530

    fbtable

--- api_web/reporting_routes.py
+++ api_web/reporting_routes.py
@@ -60,14 +60,24 @@ def get_data():
 			fbdata[date]["campaigns"][campaign_id] = {
 				"campaign_id": campaign_id,
 				"campaign_name": row[2],
-				"ad_sets": {}
+				"ad_sets": {},
+				"impression": 0,
+				"clicks": 0,
+				"spend" : 0.0,
+				"order" : 0,
+				"total" : 0.0
 			}
 
 		if ad_set_id not in fbdata[date]["campaigns"][campaign_id]["ad_sets"]:
 			fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id] = {
 				"ad_set_id": ad_set_id,
 				"ad_set_name": row[4],
-				"ads": []
+				"ads": [],
+				"impression": 0,
+				"clicks": 0,
+				"spend" : 0.0,
+				"order" : 0,
+				"total" : 0.0
 			}
 
 		fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["ads"].append({
@@ -79,48 +89,29 @@ def get_data():
 			"order": row[10],
 			"total": float(row[11])
 		})
+		
+		fbdata[date]["campaigns"][campaign_id]["impression"] = fbdata[date]["campaigns"][campaign_id]["impression"] + row[7]
+		fbdata[date]["campaigns"][campaign_id]["clicks"] = fbdata[date]["campaigns"][campaign_id]["clicks"] + row[8]
+		fbdata[date]["campaigns"][campaign_id]["spend"] = fbdata[date]["campaigns"][campaign_id]["spend"] + float(row[9])
+		fbdata[date]["campaigns"][campaign_id]["order"] = fbdata[date]["campaigns"][campaign_id]["order"] + row[10]
+		fbdata[date]["campaigns"][campaign_id]["total"] = fbdata[date]["campaigns"][campaign_id]["total"] + float(row[11])
+
+		fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["impression"] = fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["impression"] + row[7]
+		fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["clicks"] = fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["clicks"] + row[8]
+		fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["spend"] = fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["spend"] + float(row[9])
+		fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["order"] = fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["order"] + row[10]
+		fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["total"] = fbdata[date]["campaigns"][campaign_id]["ad_sets"][ad_set_id]["total"] + float(row[11])
+
 
 	# Convert the nested structure to a list of dates with campaigns
-	date_list = list(fbdata.values())
+	date_list = fbdata
 	for date_entry in date_list:
-		date_entry["campaigns"] = list(date_entry["campaigns"].values())
-		for campaign in date_entry["campaigns"]:
+		date_list[date_entry]["campaigns"] = list(date_list[date_entry]["campaigns"].values())
+		for campaign in date_list[date_entry]["campaigns"]:
 			campaign["ad_sets"] = list(campaign["ad_sets"].values())
 
 	# Convert to JSON
-	json_data = json.dumps(date_list, indent=4)
-
-
-	# Transform the result into the desired JSON format
-	# data_json = {}
-	# for row in data:
-	# 	import pdb
-	# 	pdb.set_trace()
-	# 	dated = row[0]
-	# 	if dated not in data_json:
-	# 		campaign = row[1]
-	# 		data_json[dated] = {
-	# 			'campaign': campaign,
-	# 			'adset': []
-	# 		}
-	# 	if campaign in data_json[dated]:
-	# 		adset = row[2]           
-	# 		data_json[dated][campaign] = {
-	# 			'adset': adset,
-	# 			'ad': [],					
-	# 		}
-	# 	if adset not in data_json[dated][campaign]:
-	# 		ad = row[3]           
-	# 		data_json[dated][campaign][adset] = {
-	# 			'ad': ad,					
-	# 		}
-	# 	if ad in data_json[dated][campaign][adset][ad]:
-	# 		data_json[dated][campaign][adset][ad] = {
-	# 			'impression': row[4],
-	# 			'clicks': row[5],
-	# 			'order': row[6],
-	# 			'total': row[7]
-	# 		}
+	json_data = json.dumps(date_list)
 
 
 	return jsonify(json_data)
