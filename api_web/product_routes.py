baf07d7846053d19e8fbad2537e21a50e8bdf40f
Devashish Jadoun <devashish@sannidhyabaweja.com>
2025-12-17 16:33:56 +0530

    addition of catalogue api

--- api_web/product_routes.py
+++ api_web/product_routes.py
@@ -42,7 +42,37 @@ def get_products():
         db.session.close()
 
 
-    columns = ['product_name','image_url','impressions', 'clicks', 'add_to_cart', 'purchases','spend', 'revenue', 'units', 'roas','cpc', 'cr_percent', 'c2c_percent']
+    columns = ['product_name','image_url','impressions', 'clicks', 'add_to_cart','spend', 'revenue', 'units', 'roas','cpc', 'cr_percent', 'c2c_percent']
+    result = [dict(zip(columns, row)) for row in data]
+
+    return result
+
+
+@product_bp.route('/catalogue_products', methods=['GET', 'OPTIONS'])
+@cross_origin(origins='*', methods=['GET'], headers=['Content-Type'])
+def get_catalogue_products():
+    headers = request.headers
+    body = request.args
+    startdate = body.get('startdate')
+    enddate = body.get('enddate')
+    traffic = body.get('traffic')
+    userid = headers.get('workspaceId')
+
+    
+    user = UserRegister.query.filter_by(workspace=userid).first()
+
+    try:
+        sql_query = text("SELECT * FROM get_catalogue_products_data(:workspace, :productid,  :startdate, :enddate,:channel)")
+        op = db.session.execute(sql_query, { 'workspace': userid, 'productid':user.productid, 'startdate':startdate, 'enddate':enddate,'channel':traffic})
+        data = op.fetchall()
+    except Exception as e:
+        db.session.rollback()
+        raise e
+    finally:
+        db.session.close()
+
+
+    columns = ['product_name','image_url','clicks', 'add_to_cart','spend', 'revenue', 'units', 'roas','cpc', 'cr_percent', 'c2c_percent']
     result = [dict(zip(columns, row)) for row in data]
 
     return result
